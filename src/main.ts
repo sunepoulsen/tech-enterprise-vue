f2f69c288247b564d7b4e2a59a3f03091604b500
sunepoulsen <sunepoulsen@gmail.com>
2020-03-07 18:42:35 +0100

    Add PrimeVUE component library.

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,15 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 
+import 'primevue/resources/themes/nova-light/theme.css'
+import 'primevue/resources/primevue.min.css'
+import 'primeicons/primeicons.css'
+
+// Import PrimeVUE UI components
+import Panel from 'primevue/panel'
+
+Vue.component('Panel', Panel)
+
 Vue.config.productionTip = false
 
 new Vue({
